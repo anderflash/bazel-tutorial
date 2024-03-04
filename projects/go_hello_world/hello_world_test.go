9f6fa04686452c3b2de6e8100e036d7412bb6441
Anderson Tavares <atavares@qti.qualcomm.com>
2024-03-04 15:50:51 +0100

    Adding go

---
+++ projects/go_hello_world/hello_world_test.go
@@ -0,0 +1,10 @@
+package go_hello_world
+import "testing"
+
+func TestGreeter (t *testing.T) {
+	expected := "Hello World!"
+	actual := HelloWorld()
+	if actual != expected {
+		t.Errorf("expected %q but got %q", expected, actual)
+	}
+}
